8c5f35b5abe88abb6a8a53c902aa6b293de2b92e
JD <jugendra.bhati@tevatrontech.com>
2023-07-18 17:36:52 +0530

    Listview replaced with Gridview. SharedPreferences replaced with Room Database. Some other minor functionality improved. In-App update implemented.

---
+++ app/src/main/java/com/jdgames/notes/Entity/Notes.java
@@ -0,0 +1,44 @@
+package com.jdgames.notes.Entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity
+public class Notes implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "subtitle")
+    private String subtitle;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+}
